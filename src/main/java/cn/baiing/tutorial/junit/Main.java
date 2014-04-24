fce0d3a6990ca62943be48e899b3187f4fc1ee14
sluk3r <sluk3r@gmail.com>
2014-04-24 07:18:30 +0000

    first init

---
+++ src/main/java/cn/baiing/tutorial/junit/Main.java
@@ -0,0 +1,35 @@
+package cn.baiing.tutorial.junit;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+
+/**
+ * Created by wangxc on 2014/4/24.
+ */
+
+public class Main {
+    final static  int w_unit = 10000;
+
+    final static String cellValue = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    final static String[] data = new String[]{cellValue,cellValue};
+
+
+    public static void main(String[] args) {
+        SXSSFWorkbook workbook = workbook = new SXSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Sample sheet");
+
+        int test_lines = 10 * w_unit;
+        for (int i=0;i< test_lines; i++ ) {
+            Row testRow = sheet.createRow(sheet.getLastRowNum() + 1);
+
+            for (int j = 0; j < data.length; j++) {
+                Cell testCell = testRow.createCell(j);
+                testCell.setCellValue(data[j]);
+            }
+        }
+
+        System.out.println("max line test ok, and number is: " + test_lines );
+    }
+}
